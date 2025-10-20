263b6f85904f456cf882d816dd13959595bb36f8
nianod <arnoldkk422@gmail.com>
2025-10-20 23:01:15 +0300

    removed git track rm & env

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -6,8 +6,9 @@ const Header = () => {
         image: "/logoipsum-357.svg"
     }
   return (
-    <div>
+    <div className="fixed z-30 p-3">
         <div>
+            <img src={headerItems.image} alt="Arnold" />
             <Link to="/">{headerItems.title}</Link>
         </div>
     </div>
