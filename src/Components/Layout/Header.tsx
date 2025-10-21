41ecfbb4e60394e220cff1c484cb00d895f0c055
nianod <arnoldkk422@gmail.com>
2025-10-21 12:08:46 +0300

    footers

--- src/Components/Layout/Header.tsx
+++ src/Components/Layout/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Moon, Sun } from "lucide-react"
 const Header = () => {
 
     const headerItems = {
@@ -6,10 +7,15 @@ const Header = () => {
         image: "/logoipsum-357.svg"
     }
   return (
-    <div className="fixed z-30 p-3">
-        <div>
+    <div className="fixed z-30 w-full border-b border-gray-600">
+        <div className="flex bg-[black] items-center gap-2 p-3">
             <img src={headerItems.image} alt="Arnold" />
-            <Link to="/">{headerItems.title}</Link>
+            <Link
+             to="/"
+             className="text-white"
+            >
+              {headerItems.title}
+            </Link>
         </div>
     </div>
   )
