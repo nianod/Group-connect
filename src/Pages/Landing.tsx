60c6f6bab0ca22b5e7e4e5bd567b2cd4874c6a33
nianod <arnoldkk422@gmail.com>
2025-10-23 23:14:24 +0300

    home design

--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,16 +1,41 @@
 import { ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
+  
 
 const Landing = () => {
   return (
     <div className="pt-20 text-black dark:text-white">
-      <div className="justify-end p-2 flex">
-        <Link
-          to="/register"
-          className="flex gap-3 w-fit rounded p-2 cursor-pointer font-semibold text-white bg-gray-900 dark:text-black dark:bg-white"
-        >
-          Get started <ArrowRight />
-        </Link>
+      <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+      
+        <div className="flex flex-col max-w-xl space-y-4 p-3">
+          <p className="text-xl font-semibold text-gray-600 dark:text-gray-300">
+            Find your perfect study group, learn smarter, not harder.
+          </p>
+          <h11 className="text-4xl md:text-5xl font-[helvetica] font-semibold leading-tight">
+            Connect with students who share your passion. <br /> Team up. Study better.
+          </h11>
+
+          <Link
+            to="/register"
+            className="flex items-center gap-2 w-fit rounded px-4 py-2 mt-4 font-semibold text-white bg-gray-900 dark:text-black dark:bg-white"
+          >
+            Get started <ArrowRight />
+          </Link>
+        </div>
+
+         
+        <div className="flex gap-4">
+          <img
+            src="/Preparing for the CA exams can be a daunting task….jpeg"
+            alt="Students studying together"
+            className="w-48 h-48 object-cover rounded-2xl shadow-md"
+          />
+          <img
+            src="/Studying in Library.jpeg"
+            alt="Group discussion"
+            className="w-48 h-48 object-cover rounded-2xl shadow-md mt-6"
+          />
+        </div>
       </div>
     </div>
   );
