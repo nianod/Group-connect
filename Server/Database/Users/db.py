1dc1fc32b30ec973c7b3562e5e31dd834d854cf4
nianod <arnoldkk422@gmail.com>
2025-11-03 18:50:51 +0300

    fixed #50 to insert data to mongo

--- Server/Database/Users/db.py
+++ Server/Database/Users/db.py
@@ -5,9 +5,14 @@ import os
 load_dotenv()
 
 MONGO_URL = os.getenv("MONGO_URL")
+
 client = MongoClient(MONGO_URL)
+
 db = client["group_connect"]
+
+
 users_collection = db["users"]
+groups_collection = db["group-create"]
 
 
-
+ 
